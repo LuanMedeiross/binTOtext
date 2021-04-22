c8671a855a8e50498fd2eec5ac70415c2866cbb3
LuanMedeiross <70492359+LuanMedeiross@users.noreply.github.com>
2021-04-22 19:28:02 -0300

    Update binTOtext.c

--- binTOtext.c
+++ binTOtext.c
@@ -1,29 +1,61 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 
 	int bits[8] = { 128, 64, 32, 16, 8, 4, 2, 1 };
 
-	char text[128];
-	int bin = 0;
+	printf("\n");
 
-	for (int i = 1; i < argc; i++) {
-		for (int c = 0; c <= 8; c++) {
-			if (argv[i][c] == '1') {
-				bin += bits[c];
-			}
-		} 
+	if (strcmp(argv[1], "-e")) { 
 
-		text[i-1] = bin;
-		bin = 0;
-	}
+		char text[128];
+		int bin = 0;
 
-	printf("Decimal: ");
-	for (int i = 0; i < argc - 1; i++) {
-		printf("%i ", text[i]);
-	}
+		for (int i = 1; i < argc; i++) {
+			for (int c = 0; c <= 8; c++) {
+				if (argv[i][c] == '1') {
+					bin += bits[c];
+				}
+			} 
 
-	printf("\nText: %s\n", text);
+			text[i-1] = bin;
+			bin = 0;
+		}
+
+		printf("Decimal: ");
+		for (int i = 0; i < argc - 1; i++) {
+			printf("%i ", text[i]);
+		}
+
+		printf("\nText: %s\n", text);
 	
+	} else {
+		
+		char byte[8];
+
+		for (int arg = 2; arg < argc; arg++) {
+
+			int sizeOfArg = strlen(argv[arg]);
+
+			for (int chaR = 0; chaR < sizeOfArg; chaR++) {
+
+				int decimal = argv[arg][chaR];
+				
+				for (int bit = 0; bit <= 8; bit++) {
+					if (decimal >= bits[bit]) {
+						decimal -= bits[bit];
+						byte[bit] = '1';
+					} else {
+						byte[bit] = '0';
+					}
+				} printf("%s ", byte);
+			} 
+		} printf("\n");
+	}
+		
+	printf("\n");
+
 	return 0;
+	
 }
